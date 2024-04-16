6929ae5e234ad50d1acc1ee2073d40ec70920dc2
DecayBro <lsy379945671@hotmail.com>
2024-04-16 21:08:56 +0800

    Add TrayIcon using H.NotifyIcon

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,12 +13,8 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.Win32;
-using Windows.Win32.Foundation;
-using Windows.Win32.UI.WindowsAndMessaging;
-using NotifyIcon;
-using static NotifyIcon.NotifyIconTools;
 using System.Runtime.InteropServices;
+using H.NotifyIcon;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -32,24 +28,16 @@ namespace LivelyHWTracker
         public MainWindow()
         {
             this.InitializeComponent();
-            hWndMain = WinRT.Interop.WindowNative.GetWindowHandle(this);
-            string sDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string sIconFile = sDirectory + @"Assets\butterfly.ico";
-            m_hIcon = LoadImage(IntPtr.Zero, sIconFile, IMAGE_ICON, 32, 32, LR_LOADFROMFILE);
         }
 
-        private void Window_Closed(object sender, WindowEventArgs args)
+        private void MainWindowClosed(object sender, WindowEventArgs args)
         {
-            TrayMessage(hWndMain, "Lively HWTracker", m_hIcon, IntPtr.Zero, NIM.ADD, NIIF.NONE, null, null, 0);
+            this.Hide();
+            this.HideInTaskbar();
+            trayIcon.Visibility = Visibility.Visible;
             args.Handled = true;
-            PInvoke.ShowWindow(new HWND(hWndMain), 0);
         }
 
-        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        public static extern IntPtr LoadImage(IntPtr hInst, string lpszName, UInt32 uType, int cxDesired, int cyDesired, UInt32 fuLoad);
-        public const int IMAGE_ICON = 1;
-        public const int LR_LOADFROMFILE = 0x00000010;
-        private IntPtr hWndMain = IntPtr.Zero;
-        private IntPtr m_hIcon = IntPtr.Zero;
+      
     }
 }
